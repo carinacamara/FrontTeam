96cf6bbead18d4879034ad088d370b4b0f0564ed
Carina <xd.carina95@gmail.com>
2018-08-03 15:50:44 +0300

    filled the txt file

--- hello.c
+++ hello.c
@@ -1,1 +1,15 @@
-first line
+"Baby Boy" is a song by American singer Beyoncé (pictured) from her 
+debut solo studio album Dangerously in Love. Featuring Jamaican rapper 
+Sean Paul, the song was released by Columbia Records and Music World 
+Entertainment as the second single from the album on August 3, 2003. 
+Both artists co-wrote the song with Scott Storch, Robert Waller and 
+Jay-Z; Beyoncé also co-produced the song. Containing a lyrical 
+interpolation of "No Fear" by hip hop group O.G.C, "Baby Boy" is an R&B 
+and dancehall song with reggae and Arabic music influences. The lyrics 
+detail a woman's fantasies. The song topped the US Billboard Hot 100 for 
+nine consecutive weeks, and was Beyoncé's longest-running solo 
+number-one single until 2007, when it was surpassed by "Irreplaceable". 
+It reached the top ten in many countries, and was certified platinum in 
+Australia and the US. The song's music video was directed by Jake Nava 
+and mostly shows Beyoncé dancing in various locations. "Baby Boy" has 
+remained a staple of Beyoncé's concert set lists.
